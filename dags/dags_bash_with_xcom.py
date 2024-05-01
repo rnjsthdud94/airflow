1d335742a8c6b3230a862255da84730ca94d8f65
so0ee <rnjsthdud94@naver.com>
2024-05-01 23:55:58 +0900

    오타수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -21,7 +21,7 @@ with DAG(
         task_id = 'bash_pull',
         env={'PUSHED_VALUE':"{{ ti.xcom_pull(key='bash_pushed')}}",
              'RETURN_VALUE':"{{ ti.xcom_pull(task_ids = 'bash_push')}}"},
-        bash_command="echo $PUSHED_VALUE & $RETURN_VALUE ",
+        bash_command="echo $PUSHED_VALUE && $RETURN_VALUE ",
         do_xcom_push = False
     )
 
