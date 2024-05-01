da7dfe349054468adc6f7f98abcbc5fb3d60b0e2
so0ee <rnjsthdud94@naver.com>
2024-05-01 23:03:09 +0900

    xcom 활용 test

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -9,9 +9,18 @@ with DAG(
     start_date=pendulum.datetime(2023, 3, 1, tz = "Asia/Seoul"), 
     catchup=False
 ) as dag:
-    send_email_task = EmailOperator(
-        task_id = 'send_email_task', 
+    send_email_task1 = EmailOperator(
+        task_id = 'send_email_task1', 
         to = 'choice4116@gmail.com',
         subject='Airflow 성공메일',
         html_content='조현석 사랑한다',
     )
+
+    send_email_task2 = EmailOperator(
+        task_id = 'send_email_task2', 
+        to = 'rnjsthdud94@naver.com',
+        subject='Airflow 성공메일',
+        html_content='청약통장에 돈 납입하거라',
+    )
+
+    send_email_task1 >> send_email_task2
