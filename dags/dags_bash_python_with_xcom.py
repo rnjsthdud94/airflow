c355a06acbb6729c102a355833eb0f511050b6b2
so0ee <rnjsthdud94@naver.com>
2024-06-01 16:51:38 +0900

    오타 수정

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -37,7 +37,7 @@ with DAG(
     def python_pull_xcom(**kwargs):
         ti = kwargs['ti']
         status_value = ti.xcom_pull(key = 'bash_pushed')
-        retrun_value = ti.xcom_pull(task_id = 'bash_push')
+        retrun_value = ti.xcom_pull(task_ids = 'bash_push')
         print('status_value:' + str(status_value))
         print('return_value:' + retrun_value)
     
