9052edaf6c470a47de6f64b7102277fbc992b632
so0ee <rnjsthdud94@naver.com>
2024-06-01 16:59:21 +0900

    오타 수정

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -18,9 +18,9 @@ with DAG(
     bash_pull = BashOperator(
         task_id = 'bash_pull',
         env = {
-            'STATUS':'{{ti.xcom_pull(task_id = "python_push")["status"]}}',
-            'DATA':'{{ti.xcom_pull(task_id = "python_push")["data"]}}',
-            'OPTIONS_CNT':'{{ti.xcom_pull(task_id = "python_push")["options_cnt"]}}'
+            'STATUS':'{{ti.xcom_pull(task_ids = "python_push")["status"]}}',
+            'DATA':'{{ti.xcom_pull(task_ids = "python_push")["data"]}}',
+            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids = "python_push")["options_cnt"]}}'
         },
         bash_command='echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
     )
